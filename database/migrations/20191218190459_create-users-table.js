8caeb096846edecb18117b001e0507a21a91ac46
Heather-Ridgill <Heather.Ridgill@gmail.com>
2020-01-07 20:30:57 -0800

    updated README and Migrations

--- database/migrations/20191218190459_create-users-table.js
+++ database/migrations/20191218190459_create-users-table.js
@@ -5,13 +5,14 @@ exports.up = function(knex) {
  
       tbl.string('username', 255).notNullable();
         tbl.string('password', 255).notNullable();
+        tbl.string('confirm', 255).notNullable();
         tbl.string('first_name', 255).notNullable();
         tbl.string('last_name', 255).notNullable();
         tbl.string('street_address', 255).notNullable();
         tbl.string('city', 255).notNullable();
         tbl.string('state', 255).notNullable();
         tbl.string('zip', 255).notNullable();
-        tbl.string('phone_number', 255).notNullable()
+        tbl.string('phone_number', 255).notNullable();
         tbl.string('home_airport', 255).notNullable();
         tbl.boolean('admin').defaultTo(false);
     });
